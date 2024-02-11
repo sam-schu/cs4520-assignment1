cf77ac7d5e7a228087fa6421a454d059dcb2c7b4
Sam <shortdriva@aol.com>
2024-02-10 21:24:17 -0500

    added main activity layout with navigation host fragment

--- app/src/main/java/com/cs4520/assignment1/ui/MainActivity.kt
+++ app/src/main/java/com/cs4520/assignment1/ui/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.cs4520.assignment1.ui
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.cs4520.assignment1.R
 
 class MainActivity : AppCompatActivity() {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
 }
