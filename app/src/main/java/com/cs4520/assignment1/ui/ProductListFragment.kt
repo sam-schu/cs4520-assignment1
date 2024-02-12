bb083b32f91fdf825cd4de58f4fa25f38626657d
Sam <shortdriva@aol.com>
2024-02-11 20:40:07 -0500

    added recycler view with placeholder numeric list items

--- app/src/main/java/com/cs4520/assignment1/ui/ProductListFragment.kt
+++ app/src/main/java/com/cs4520/assignment1/ui/ProductListFragment.kt
@@ -1,12 +1,15 @@
 package com.cs4520.assignment1.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.cs4520.assignment1.databinding.ProductListFragmentBinding
+import com.cs4520.assignment1.databinding.ProductListItemBinding
+import com.cs4520.assignment1.logic.Product
 import com.cs4520.assignment1.logic.ProductManager
 import com.cs4520.assignment1.productsDataset
 
@@ -25,7 +28,44 @@ class ProductListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        productManager = ProductManager()
-        productManager.importProductData(productsDataset)
+        productManager = ProductManager().apply { importProductData(productsDataset) }
+
+        with (binding.recyclerView) {
+            layoutManager = LinearLayoutManager(activity)
+            adapter = RecyclerViewAdapter(productManager.getAllProducts())
+        }
     }
+}
+
+private class RecyclerViewAdapter(private val products: List<Product>) :
+    RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View, val binding: ProductListItemBinding) :
+        RecyclerView.ViewHolder(view) {
+
+        fun bind(test: Int) {
+            binding.test.text = test.toString()
+        }
+    }
+
+    /*class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val textView = view.findViewById<TextView>(R.id.test)
+        fun bind(test: Int) {
+            textView.text = test.toString()
+        }
+    }*/
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        ProductListItemBinding.inflate(LayoutInflater.from(parent.context)).let {
+            return ViewHolder(it.root, it)
+        }
+        /*val view = LayoutInflater.from(parent.context).inflate(R.layout.product_list_item, parent, false)
+        return ViewHolder(view)*/
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    override fun getItemCount(): Int = products.size * 10
 }
